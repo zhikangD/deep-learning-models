0ad42fa795faf987ead6815ad43ac3aac584fc66
zhikangD <zhikang@photobutler.com>
2018-03-19 17:36:13 -0400

    1

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import sys
 import argparse
 import _pickle
 from resnet50 import ResNet50
+from vgg16 import VGG16
 from keras.applications.imagenet_utils import preprocess_input
 from keras.preprocessing import image
 from keras.layers import GlobalAveragePooling2D, Dense, Dropout,Activation,Flatten
@@ -23,28 +24,29 @@ def parse_args(args):
     parser.add_argument('--data_dir', help='Path to dataset directory.')
     parser.add_argument('--epochs', help='Number of epochs to train.', type=int, default=20)
     parser.add_argument('--usepkldata',action='store_true', help='use data from saved pickle file or create from image.')
+    parser.add_argument('--model', default='resnet')
     return parser.parse_args(args)
 
-def f1_score(y_true, y_pred):
-
-    # Count positive samples.
-    c1 = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    c2 = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    c3 = K.sum(K.round(K.clip(y_true, 0, 1)))
-
-    # If there are no true samples, fix the F1 score at 0.
-    if c3 == 0:
-        return 0
-
-    # How many selected items are relevant?
-    precision = c1 / c2
-
-    # How many relevant items are selected?
-    recall = c1 / c3
-
-    # Calculate f1_score
-    f1_score = 2 * (precision * recall) / (precision + recall)
-    return f1_score
+# def f1_score(y_true, y_pred):
+#
+#     # Count positive samples.
+#     c1 = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+#     c2 = K.sum(K.round(K.clip(y_pred, 0, 1)))
+#     c3 = K.sum(K.round(K.clip(y_true, 0, 1)))
+#
+#     # If there are no true samples, fix the F1 score at 0.
+#     if c3 == 0:
+#         return 0
+#
+#     # How many selected items are relevant?
+#     precision = c1 / c2
+#
+#     # How many relevant items are selected?
+#     recall = c1 / c3
+#
+#     # Calculate f1_score
+#     f1_score = 2 * (precision * recall) / (precision + recall)
+#     return f1_score
 
 def main(args=None):
     # parse arguments
@@ -93,30 +95,62 @@ def main(args=None):
     # Split the dataset
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
     ###########################################################################################################################
-    # Training the classifier alone
-    image_input = Input(shape=(224, 224, 3))
+    if args.model=='resnet':
+        # Training the classifier alone
+        image_input = Input(shape=(224, 224, 3))
 
-    model = ResNet50(input_tensor=image_input, weights='imagenet')
-    model.summary()
-    last_layer = model.get_layer('avg_pool').output
-    x = Flatten(name='flatten')(last_layer)
-    out = Dense(num_classes, activation='softmax', name='output_layer')(x)
-    custom_resnet_model = Model(inputs=image_input, outputs=out)
-    custom_resnet_model.summary()
-    for layer in custom_resnet_model.layers[:-1]:
-        layer.trainable = False
-    custom_resnet_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', f1_score])
-    t = time.time()
-    filepath = "./weights-improvement-{epoch:02d}-{val_acc:.2f}.h5"
-    checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-    callbacks_list = [checkpoint]
-    hist = custom_resnet_model.fit(X_train, y_train, batch_size=32, epochs=args.epochs, verbose=1,
-                                   validation_data=(X_test, y_test),
-                                   callbacks=callbacks_list)
-    print('Training time: %s' % (t - time.time()))
-    (loss, accuracy) = custom_resnet_model.evaluate(X_test, y_test, batch_size=10, verbose=1)
+        model = ResNet50(input_tensor=image_input, weights='imagenet')
+        model.summary()
+        last_layer = model.get_layer('avg_pool').output
+        x = Flatten(name='flatten')(last_layer)
+        out = Dense(num_classes, activation='softmax', name='output_layer')(x)
+        custom_resnet_model = Model(inputs=image_input, outputs=out)
+        custom_resnet_model.summary()
+        for layer in custom_resnet_model.layers[:-1]:
+            layer.trainable = False
+        custom_resnet_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', f1_score])
+        t = time.time()
+        filepath = "./data/resnet-improvement-{epoch:02d}-{val_acc:.2f}.h5"
+        checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+        callbacks_list = [checkpoint]
+        hist = custom_resnet_model.fit(X_train, y_train, batch_size=32, epochs=args.epochs, verbose=1,
+                                       validation_data=(X_test, y_test),
+                                       callbacks=callbacks_list)
+        print('Training time: %s' % (t - time.time()))
+        (loss, accuracy) = custom_resnet_model.evaluate(X_test, y_test, batch_size=10, verbose=1)
 
-    print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss, accuracy * 100))
+        print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss, accuracy * 100))
+    #########################################################################################
+    elif args.model=='vgg':
+        # Custom_vgg_model_1
+        # Training the classifier alone
+        image_input = Input(shape=(224, 224, 3))
+
+        model = VGG16(input_tensor=image_input, include_top=True, weights='imagenet')
+        model.summary()
+        last_layer = model.get_layer('fc2').output
+        # x= Flatten(name='flatten')(last_layer)
+        out = Dense(num_classes, activation='softmax', name='output')(last_layer)
+        custom_vgg_model = Model(image_input, out)
+        custom_vgg_model.summary()
+
+        for layer in custom_vgg_model.layers[:-1]:
+            layer.trainable = False
+
+        filepath = "./data/vgg16-improvement-{epoch:02d}-{val_acc:.2f}.h5"
+        checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+        callbacks_list = [checkpoint]
+
+        custom_vgg_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+        t = time.time()
+        #	t = now()
+        hist = custom_vgg_model.fit(X_train, y_train, batch_size=32, epochs=args.epochs, verbose=1, validation_data=(X_test, y_test),
+                                    callbacks=callbacks_list)
+        print('Training time: %s' % (t - time.time()))
+        (loss, accuracy) = custom_vgg_model.evaluate(X_test, y_test, batch_size=10, verbose=1)
+
+        print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss, accuracy * 100))
 
 
 if __name__ == '__main__':
